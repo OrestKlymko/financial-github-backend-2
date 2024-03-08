1f69ca4cff09e488c75a6a7397f173643807c97d
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2024-03-08 08:45:53 +0100

    create

--- src/main/java/org/study/financebackend/security/AuthController.java
+++ src/main/java/org/study/financebackend/security/AuthController.java
@@ -28,7 +28,7 @@ public class AuthController {
 	private String clientSecret;
 
 //	private static final String URL_REDIRECT="https://financial-tracker-frontend.onrender.com";
-	private static final String URL_REDIRECT="https://microservice-production-c110.up.railway.app/";
+	private static final String URL_REDIRECT="https://microservice-production-c110.up.railway.app";
 
 
 	@GetMapping("/auth/url")
